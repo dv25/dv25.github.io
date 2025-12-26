51b4c662173d9a939eb7e6b57460c445c927eb43
Luc <sdfds@sdfsdfsd.it>
2025-12-26 17:03:20 +0100

    asd

--- service-worker.js
+++ service-worker.js
@@ -10,7 +10,7 @@ if (unoConfig.environmentVariables["UNO_BOOTSTRAP_DEBUGGER_ENABLED"] !== "True")
     self.addEventListener('install', function (e) {
         console.debug('[ServiceWorker] Installing offline worker');
         e.waitUntil(
-            caches.open('4d7fefda-1a6c-4a34-a10d-8620dbb0dc91').then(async function (cache) {
+            caches.open('37a01b5f-6b49-4c11-a035-c3aae60fd78f').then(async function (cache) {
                 console.debug('[ServiceWorker] Caching app binaries and content');
 
                 // Add files one by one to avoid failed downloads to prevent the
@@ -97,7 +97,7 @@ if (unoConfig.environmentVariables["UNO_BOOTSTRAP_DEBUGGER_ENABLED"] !== "True")
             caches.keys().then(function (cacheNames) {
                 return Promise.all(
                     cacheNames.filter(function (cacheName) {
-                        return cacheName !== '4d7fefda-1a6c-4a34-a10d-8620dbb0dc91';
+                        return cacheName !== '37a01b5f-6b49-4c11-a035-c3aae60fd78f';
                     }).map(function (cacheName) {
                         console.debug('[ServiceWorker] Deleting old cache:', cacheName);
                         return caches.delete(cacheName);
