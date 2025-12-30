b5231e3b4f7281fe7bbe1b0821e1022e8b50e05e
Luc <sdfds@sdfsdfsd.it>
2025-12-30 16:26:54 +0100

    sdfdsf

--- package_828c0149746a23d1054be90710bba2ae225e6e66/AppManifest.js
+++
@@ -1,7 +0,0 @@
-var UnoAppManifest = {
-    lightThemeBackgroundColor: "#f00",
-    darkThemeBackgroundColor: "#0f0",
-    displayName: "Septimus",
-    splashScreenImage: "splash_screen.scale-200.png",
-    splashScreenColor: "#ffffff",
-}
